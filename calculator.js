a2006f5454cedb0ba59eca31e6fae3be591ec545
bnestes18 <bnestes2014@gmail.com>
2020-02-17 12:07:48 -0500

    Add extra comments for clarity

--- calculator.js
+++ calculator.js
@@ -1,23 +1,31 @@
 // Computes Mortgage Formula
 function calculateMortgage() {
-    let principal = document.getElementById('amount-text').value;
-    let ratePercent = (document.getElementById('rate-text').value / 100);
-    let rate = (ratePercent / 12);
-    let years = document.getElementById('period-amount').value;
-    let numOfPayments  = years * 12;
-    
-    // Checks for empty values
-    if (principal == "" || rate == "") {
-        alert("Please enter valid value in each text field");
-    } else {
-        let monthlyPayments = (principal) * ((rate)*(Math.pow((1 + rate), numOfPayments))) / ((Math.pow((1 + rate), numOfPayments)) - 1);
-        let totalCost = monthlyPayments * numOfPayments
-        document.getElementById('total-cost').textContent = `$${totalCost.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}`
-        document.getElementById('payments').textContent = `$${monthlyPayments.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}`
-    
-    }
+	// Loan amount
+	let principal = document.getElementById('amount-text').value;
+	// Rate in decimal form
+	let ratePercent = document.getElementById('rate-text').value / 100;
+	// Interest rate (monthly)
+	let rate = ratePercent / 12;
+	// Mortgage Period (years)
+	let period = document.getElementById('period-amount').value;
+	// Num of payments
+	let numOfPayments = period * 12;
+
+	// Checks for empty values
+	if (principal == '' || rate == '') {
+		alert('Please enter valid value in each text field');
+	} else {
+		let monthlyPayments =
+			principal * (rate * Math.pow(1 + rate, numOfPayments)) / (Math.pow(1 + rate, numOfPayments) - 1);
+		let totalCost = monthlyPayments * numOfPayments;
+		document.getElementById('total-cost').textContent = `$${totalCost.toLocaleString('en', {
+			maximumFractionDigits: 2,
+		})}`;
+		document.getElementById('payments').textContent = `$${monthlyPayments.toLocaleString('en', {
+			maximumFractionDigits: 2,
+		})}`;
+	}
 }
 
 // Add event listener for Calculate Button
 document.getElementById('calc-btn').addEventListener('click', calculateMortgage);
-
